e720dde06ce6d14934fabfd37e267e3f54fe250a
bufferbloat <wayclientdsq@gmail.com>
2026-03-10 04:02:59 +0100

    Add files via upload

--- output/cs/steamaudio_dll.cs
+++ output/cs/steamaudio_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-04 23:14:20.602919400 UTC
+// 2026-03-10 03:01:19.251834800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: steamaudio.dll
