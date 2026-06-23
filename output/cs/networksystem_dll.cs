7629423cb104b11d05cfc1e588292fd5a265e91b
bufferbloat <wayclientdsq@gmail.com>
2026-06-23 15:34:00 +0200

    update

--- output/cs/networksystem_dll.cs
+++ output/cs/networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-29 01:23:35.504546700 UTC
+// 2026-06-23 13:30:58.864574900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
